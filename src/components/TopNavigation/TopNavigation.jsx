1bd00240fae04b8cd67e1c4bff9d2be2421bbd18
Modestas Skirius <modestas.skirius@gmail.com>
2021-11-26 11:53:34 +0200

    Add TopNavigation Component

---
+++ src/components/TopNavigation/TopNavigation.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import './TopNavigation.css';
+import Logo from '../../assets/Logo.svg';
+
+function TopNavigation() {
+    return (
+        <nav className="top-nav">
+            <div className="top-nav__logo">
+                <a href="#">
+                    <img src={Logo} />
+                </a>
+            </div>
+            <div className="top-nav--links">
+                <NavLink to="/" className="top-nav__link">
+                    Funkcijos
+                </NavLink>
+                <NavLink to="#" className="top-nav__link">
+                    Ataskaitos
+                </NavLink>
+                <NavLink to="#" className="top-nav__link">
+                    Sandėlys
+                </NavLink>
+                <NavLink to="#" className="top-nav__link">
+                    Nustatymai
+                </NavLink>
+                <NavLink to="#" className="top-nav__link">
+                    Atsijungti
+                </NavLink>
+            </div>
+        </nav>
+    );
+}
+
+export default TopNavigation;
