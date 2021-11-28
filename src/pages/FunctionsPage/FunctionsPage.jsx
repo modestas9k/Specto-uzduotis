6ed3291ee027313d30dae03d0318e15fbf8244a2
Modestas Skirius <modestas.skirius@gmail.com>
2021-11-28 23:58:47 +0200

    change static page to components

--- src/pages/FunctionsPage/FunctionsPage.jsx
+++ src/pages/FunctionsPage/FunctionsPage.jsx
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from 'react';
 import './FunctionsPage.css';
 import { ReactComponent as AddPeople } from '../../assets/addPeople.svg';
 import { ReactComponent as AddFirm } from '../../assets/addFirm.svg';
@@ -6,181 +7,148 @@ import { ReactComponent as Idea } from '../../assets/idea.svg';
 import { ReactComponent as Computer } from '../../assets/computer.svg';
 import { ReactComponent as Back } from '../../assets/back.svg';
 import { ReactComponent as Info } from '../../assets/info.svg';
-import { ReactComponent as Logo } from '../../assets/logo.svg';
-import { ReactComponent as World } from '../../assets/world.svg';
+import { ReactComponent as Loading } from '../../assets/time.svg';
+import { ActionButton, AsideMenu, ClientsTable, Footer, Search, Wrapper } from '../../components/';
+
+const clientsExampleData = [
+    {
+        id: 1,
+        name: 'Vardenis',
+        surname: 'Pavardenis',
+        city: 'Kaunas',
+        cardNumber: 23235323,
+        ID: 1233213213,
+        personalCode: 34490432432,
+        bornDate: 1920,
+        phone1: '+370 000 00000',
+        phone2: '+370 000 00000',
+    },
+    {
+        id: 2,
+        name: 'Vardenis2',
+        surname: 'Pavardenis2',
+        city: 'Kaunas',
+        cardNumber: 23235323,
+        ID: 1233213213,
+        personalCode: 34490432432,
+        bornDate: 1923,
+        phone1: '+370 000 00000',
+        phone2: '+370 000 00000',
+    },
+];
 
 function FunctionsPage() {
+    const [client, setClient] = useState('');
+    const [searchValue, setSearchValue] = useState('');
+    const [clientsData, setClientsData] = useState('');
+
+    const [loadingClient, setLoadingClient] = useState(false);
+
+    const selectedClient = (childData) => {
+        const client = clientsExampleData.filter((client) => client.id === childData);
+        setClient(client);
+    };
+    const searchInputData = (childData) => {
+        setSearchValue(childData);
+    };
+    useEffect(() => {
+        setLoadingClient(true);
+        const timer = setTimeout(() => {
+            setLoadingClient(false);
+        }, 1000);
+        return () => clearTimeout(timer);
+    }, [client]);
+
+    useEffect(() => {
+        if (searchValue) {
+            const results = clientsExampleData.filter((client) =>
+                client.name.toLowerCase().includes(searchValue)
+            );
+            setClientsData(results);
+        } else {
+            setClientsData(clientsExampleData);
+        }
+    }, [searchValue]);
+
     return (
         <div className="main-container">
-            <asside className="asside__container">
-                <div className="asside__nav">
-                    <h3>Dienos Funkcijos</h3>
-                    <ul>
-                        <li>Registracija</li>
-                        <li className="active-list-item">Kliento Paieška</li>
-                        <li>Sąskaitos</li>
-                        <li>Kasa</li>
-                        <li>Orderiai</li>
-                        <li>Medžiagų išdavimas</li>
-                    </ul>
-                </div>
-                <div className="asside__nav">
-                    <h3>Kortelės</h3>
-                    <ul>
-                        <li>Fiziniai klientai</li>
-                        <li>Juridiniai klientai</li>
-                        <li>Prekių tiekėjai</li>
-                        <li>Paslaugos</li>
-                        <li>Prekės</li>
-                        <li>Darbuotojai</li>
-                    </ul>
-                </div>
-            </asside>
+            <AsideMenu />
 
-            <main className="main-view">
-                <div className="top-buttons-wrapper">
-                    <a>
-                        <div className="svg-wrapper">
-                            <AddPeople className="svg" />
-                        </div>
-                    </a>
-                    <a>
-                        <div className="svg-wrapper">
-                            <AddFirm className="svg" />
-                        </div>
-                    </a>
-                    <a>
-                        <div className="svg-wrapper">
-                            <Print className="svg" />
-                        </div>
-                    </a>
-                    <a>
-                        <div className="svg-wrapper">
-                            <Idea className="svg" />
-                        </div>
-                    </a>
-                    <a>
-                        <div className="svg-wrapper">
-                            <Computer className="svg" />
-                        </div>
-                    </a>
-                    <a>
-                        <div className="svg-wrapper">
-                            <Back className="svg" />
-                        </div>
-                    </a>
-                </div>
-                <div className="wrapper">
-                    <h1>Kliento paieška</h1>
-                    <div className="client-search-table">
-                        {/* Search Input */}
-                        <div>
-                            <input
-                                id="search"
-                                className="search-input"
-                                type="search"
-                                placeholder="Kliento paieška"
-                            />
-                        </div>
-                        {/* Clients Table */}
-                        <table className="clients-table">
-                            <tr>
-                                <th>Vardas</th>
-                                <th>Pavardė</th>
-                                <th>Miestas</th>
-                                <th>Kortelės NR</th>
-                                <th>Kliento ID</th>
-                                <th>Asmens kodas</th>
-                                <th>Gimes</th>
-                                <th>Telefono nr. 1</th>
-                                <th>Telefono nr. 2</th>
-                            </tr>
-                            <tr>
-                                <td>Vardenis</td>
-                                <td>Pavardenis</td>
-                                <td>Kaunas</td>
-                                <td>21 V00001</td>
-                                <td>0000000000</td>
-                                <td>00000000000</td>
-                                <td>1974</td>
-                                <td>+370 000 00000</td>
-                                <td>+370 000 00000</td>
-                            </tr>
-                            <tr className="client-table__client-row">
-                                <td>Vardenis</td>
-                                <td>Pavardenis</td>
-                                <td>Kaunas</td>
-                                <td>21 V00001</td>
-                                <td>0000000000</td>
-                                <td>00000000000</td>
-                                <td>1974</td>
-                                <td>+370 000 00000</td>
-                                <td>+370 000 00000</td>
-                            </tr>
-                        </table>
+            <div className="main-view">
+                <div>
+                    <div className="top-buttons-wrapper">
+                        <ActionButton Icon={AddPeople} tooltip="Naujas žmogus" />
+                        <ActionButton Icon={AddFirm} tooltip="Nauja įmonė" />
+                        <ActionButton Icon={Print} tooltip="Atspausdinti" />
+                        <ActionButton Icon={Idea} tooltip="Daugiau informacijos" />
+                        <ActionButton Icon={Computer} tooltip="Nustatymai" />
+                        <ActionButton Icon={Back} tooltip="Atgal" />
                     </div>
-                </div>
-                <div className="wrapper">
-                    <h1>Vardenis Pavardenis</h1>
-                    <div className="client-search-table client-wrapper">
-                        <div className="client">
-                            <h4>Svarbi informacija</h4>
-                            <div className="client__info">
-                                <div className="">
-                                    <h5>
-                                        Neatvyko po užsakymo patvirtinimo: <span>0</span> kartai
-                                    </h5>
-                                </div>
-                                <div>
-                                    <a>
-                                        <div className="svg-wrapper">
-                                            <Info className="svg" />
-                                        </div>
-                                    </a>
-                                    <a>
-                                        <div className="svg-wrapper">
-                                            <Info className="svg" />
-                                        </div>
-                                    </a>
-                                </div>
+
+                    <Wrapper title="Kliento paieška">
+                        <div>
+                            <Search searchInputData={searchInputData} />
+
+                            <ClientsTable clients={clientsData} selectedClient={selectedClient} />
+                        </div>
+                    </Wrapper>
+                    {loadingClient && client ? (
+                        <div className="loading">
+                            <div>
+                                <span>Informacija kraunasi</span>
+                                <Loading className="loading__icon" />
                             </div>
                         </div>
+                    ) : (
+                        ''
+                    )}
+                    {client &&
+                        loadingClient == false &&
+                        client.map((client) => {
+                            return (
+                                <Wrapper key={client.id} title={`${client.name} ${client.surname}`}>
+                                    <div className="client-wrapper">
+                                        <div className="client">
+                                            <h4>Svarbi informacija</h4>
+                                            <div className="client__info">
+                                                <div className="">
+                                                    <h5>
+                                                        Neatvyko po užsakymo patvirtinimo:{' '}
+                                                        <span className="client__info--span">
+                                                            0
+                                                        </span>{' '}
+                                                        kartai
+                                                    </h5>
+                                                </div>
+                                                <div>
+                                                    <ActionButton Icon={Info} />
+                                                    <ActionButton Icon={Info} />
+                                                </div>
+                                            </div>
+                                        </div>
 
-                        <div className="client__menu">
-                            <h4>Kortelė</h4>
-                            <ul>
-                                <li>Bendra informacija</li>
-                                <li>Užsakymai</li>
-                                <li>Gydytojo įrašai</li>
-                                <li>Gydymo planai</li>
-                                <li>Nuotraukos</li>
-                                <li>Receptai</li>
-                                <li>Injekcijos</li>
-                                <li>TLK</li>
-                                <li>Svarbi informacija</li>
-                                <li>SMS nustatymai</li>
-                            </ul>
-                        </div>
-                    </div>
+                                        <div className="client__menu">
+                                            <h4>Kortelė</h4>
+                                            <ul>
+                                                <li>Bendra informacija</li>
+                                                <li>Užsakymai</li>
+                                                <li>Gydytojo įrašai</li>
+                                                <li>Gydymo planai</li>
+                                                <li>Nuotraukos</li>
+                                                <li>Receptai</li>
+                                                <li>Injekcijos</li>
+                                                <li>TLK</li>
+                                                <li>Svarbi informacija</li>
+                                                <li>SMS nustatymai</li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </Wrapper>
+                            );
+                        })}
                 </div>
-                <footer>
-                    <div>
-                        <Logo className="footer__logo" />
-                        <div className="footer__links">
-                            <a href="#">Privatumo politika</a>
-                            <span>|</span>
-                            <a href="#">Teisinės nuostatos</a>
-                            <span>|</span>
-                            <a href="#">Pagalba ir kontaktai</a>
-                        </div>
-                    </div>
-                    <div>
-                        <button className="language-btn">
-                            <World className="language-btn__svg" /> Lietuvių
-                        </button>
-                    </div>
-                </footer>
-            </main>
+                <Footer />
+            </div>
         </div>
     );
 }
