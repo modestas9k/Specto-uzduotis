1d88047c2a58957e8c8c74ab018a12e309314865
Modestas Skirius <modestas.skirius@gmail.com>
2021-11-28 15:39:35 +0200

    rename page component

--- src/Routes.js
+++ src/Routes.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import { TopNavigation } from './components';
-import { Functions } from './pages';
+import { FunctionsPage } from './pages';
 
 function App() {
     return (
         <Router>
             <TopNavigation />
             <Routes>
-                <Route path="/" element={<Functions />} />
+                <Route path="/" element={<FunctionsPage />} />
             </Routes>
         </Router>
     );
