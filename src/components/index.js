e4e743d2d88deee085d4c25042a1a6b8cbfaf9b5
Modestas Skirius <modestas.skirius@gmail.com>
2021-11-28 23:52:19 +0200

    export default components

--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,7 @@
 export { default as TopNavigation } from './TopNavigation/TopNavigation';
+export { default as Footer } from './Footer/Footer';
+export { default as AsideMenu } from './AsideMenu/AsideMenu';
+export { default as ActionButton } from './ActionButton/ActionButton';
+export { default as Wrapper } from './Wrapper/Wrapper';
+export { default as Search } from './Search/Search';
+export { default as ClientsTable } from './ClientsTable/ClientsTable';
